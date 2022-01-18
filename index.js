d01973b20e456b1dc9e289e2fb3930141c0cb3f4
Ibaihima <ibaihima@hotmail.com>
2022-01-18 16:25:03 -0500

    not done

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your solution in this file!
+    const employee = {
+        name: "Sam",
+        streetAddress: "11 Broadway"
+    }
+    function updateEmployeeWithKeyAndValue(employee, key, value){
+        Object.keys(employee);
+    }
