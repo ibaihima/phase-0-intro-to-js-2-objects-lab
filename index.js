b89b6988ecdbe1ebdbb342658b8a1ed8d812dc12
Ibaihima <ibaihima@hotmail.com>
2022-01-18 21:12:47 -0500

    ALMOST DONE

--- index.js
+++ index.js
@@ -4,5 +4,27 @@
         streetAddress: "11 Broadway"
     }
     function updateEmployeeWithKeyAndValue(employee, key, value){
-        Object.keys(employee);
+        return {
+            ...employee,[key]:value
+        }
+    }
+    function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
+        employee.streetAddress = "12 Broadway";
+        return {
+            ...employee
+        }
+    }
+    function deleteFromEmployeeByKey(emoloyee, key){
+        const newEmployee = employee["name"]
+        delete newEmployee.name;
+        return {
+            ...newEmployee
+        }
+    }
+    function destructivelyDeleteFromEmployeeByKey(employee, key){
+        const newEmploye = employee["name"]
+        delete employee["name"];
+        return {
+            ...employee
+        }
     }
